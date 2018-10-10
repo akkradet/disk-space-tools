50c5ddd84be163c16a80c8941b1808710c646719
Ashley Cawley <ash@ashleycawley.co.uk>
2018-10-10 21:27:11 +0100

    Added check for available space

--- dstools.sh
+++ dstools.sh
@@ -4,9 +4,13 @@ set -u
 
 # Functions
 function RMLOG10MB {
-find /home*/*/ -type f -name "error_log" -size +10000k -exec rm -f {} \;
+    find /home*/*/ -type f -name "error_log" -size +10000k -exec rm -f {} \;
 }
 
+#function FREESPACE {
+#    df -h | grep dev | awk ' {print $4} '
+#}
+FREESPACE=$(df -h | grep dev | awk ' {print $4} ')
 # View disk usage in /var/log/ & /root/ & /home/swb/
 # Clear old archived logs from /var/log/
 # Empty all cPanel Trash Folders
@@ -17,13 +21,17 @@ find /home*/*/ -type f -name "error_log" -size +10000k -exec rm -f {} \;
 # 
 #
 
-OPTION=$(whiptail --title "Disk Space Tools" --menu "Choose your option" 15 60 8 \
+OPTION=$(whiptail --title "Disk Space Tools"  --menu "Available Space: $FREESPACE" 20 60 10 \
 "1" "Clear old / archived logs from /var/log/" \
 "2" "Empty all cPanel Trash Folders" \
 "3" "Delete all error_log files larger than 5MB" \
 "4" "See Installatron Backup folder usage" \
 "5" "View & Reduce number of Reserved Blocks on disk" \
-"6" "Exit"  3>&1 1>&2 2>&3)
+"6" "View & Reduce number of Reserved Blocks on disk" \
+"7" "View & Reduce number of Reserved Blocks on disk" \
+"8" "View & Reduce number of Reserved Blocks on disk" \
+"9" "View & Reduce number of Reserved Blocks on disk" \
+"10" "Exit"  3>&1 1>&2 2>&3)
 
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
